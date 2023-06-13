3dfb1f5c9c2194983e54b23f378a4dc56e49e87f
Xubiod <xubiod.mail@gmail.com>
2023-06-13 12:14:28 -0400

    some little cleanup

--- main.go
+++ main.go
@@ -7,49 +7,60 @@ import (
 	"os"
 )
 
-func main() {
+func prg() (err error) {
 	if len(os.Args[1:]) != 1 {
 		log.Fatalln("missing input filename")
+		return
 	}
 
 	f, err := os.Open(os.Args[1])
 	if err != nil {
-		log.Fatalln(err)
+		return
 	}
 
 	n, err := os.Create("result.json")
 	if err != nil {
-		log.Fatalln(err)
+		return
 	}
 
 	var result interface{}
 
 	data, err := io.ReadAll(f)
 	if err != nil {
-		log.Fatalln(err)
+		return
 	}
 
 	err = json.Unmarshal(data, &result)
 	if err != nil {
-		log.Fatalln(err)
+		return
 	}
 
 	data, err = json.MarshalIndent(result, "", "\t")
 	if err != nil {
-		log.Fatalln(err)
+		return
 	}
 
-	n.Write(data)
+	_, err = n.Write(data)
+	if err != nil {
+		return
+	}
 
 	err = f.Close()
 	if err != nil {
-		log.Fatalln(err)
+		return
 	}
 
 	err = n.Close()
 	if err != nil {
-		log.Fatalln(err)
+		return
 	}
 
 	log.Println("complete")
+	return
+}
+
+func main() {
+	if err := prg(); err != nil {
+		log.Fatalln(err)
+	}
 }
